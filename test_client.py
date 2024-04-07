1d36ae62a2b8714d279c41b4ed324bac29202e3c
gtg105i <gtg105i@gatech.edu>
2024-04-07 16:02:42 -0400

    added threaded connections for server

--- test_client.py
+++ test_client.py
@@ -12,4 +12,52 @@ grow
 
 import socket
 
+HOST='localhost'
 PORT=54321
+
+def main():
+	print('calling server')
+	print(request_state())
+	print('exiting')
+
+def request_state():
+	"""
+	request_pic:
+	1) initiates a socket connection with the pi_cam host
+	2) waits and recieves RGB array data back from host
+	3) converts to jpg formt
+	4) writes to a date/time stamped file
+	5) returns the file name
+	
+	returns: (string) filename 
+	"""
+	
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		
+		#ask for a picture from the server
+		print("Initiating connection")
+		s.connect((HOST,PORT))
+		s.sendall(b'Please Send me the state')
+		
+		#once we connect, recieve the response until connection is closed
+		data=0
+		data=s.recv(1024)	#our buffer is 1024 bytes
+		i=0
+		print ('Receiving state')
+		while 1:
+			#print('Receiving %d'%i)
+			tdata=s.recv(1024)
+			if not tdata: break	#if there's nothing here, break out
+			data+=tdata
+			i+=1
+		print("State %d received"%i)
+		
+		
+		
+		s.close()	#close the socket
+	
+	#return the file name
+	return data
+	
+if __name__=="__main__":
+	main()
--- test_server.py
+++ test_server.py
@@ -1,6 +1,6 @@
 '''
 This is a test program to act as server for basic requests
-
+this will now add threads to handle multiple clients
 server (this device):
 Client: don't care
 
@@ -11,29 +11,63 @@ grow
 '''
 
 import socket
+import threading
+import time
 
 HOST=''
 PORT=54321
 
+def handle_client(conn,addr):
+	'''
+		this function is what each thread executes when a client connection is made
+	'''
+	print("[thread] starting")
+	
+    # recv message
+	message = conn.recv(1024)
+	message = message.decode()
+	print("[thread] client:", addr, 'recv:', message)
+	
+	# simulate longer work, this would be where we do an update or something
+	time.sleep(5)
+
+	# send answer
+	message = "Got your message friend!"
+	message = message.encode()
+	conn.send(message)
+	print("[thread] client:", addr, 'send:', message)
+	
+	#wrap up our connection
+	conn.close()
+
+	print("[thread] ending")
+	
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:	#AF_INET = AddressFamily Internet (IPV4), SOCK_STREAM = TCP
 	s.bind((HOST,PORT))
-	s.listen()	#bind and listen on the port we initiated
-	
-	while True:
-		#Wait for a connection
-		print("waiting")
-		conn,add = s.accept()	#accept our connection and address of what we accepted, the program blocks on this line to a connection is received
-		print("accepting from ",add)
-		print("received: ",conn.recv(1024))
-		
-		#once a request has occured,
-		#we take the picture
-		data="HELLO FRIEND"
-		
-		with conn:
-			#we respond back over the socket with the data
-			print("replying with data")
-			print(conn.sendall(data))
-			print("Data sent")
+	s.listen(9)	#bind and listen on the port we initiated, will listen for 9 connections
+	all_threads=[]	#this will hold all of our threads for record keeping
+	try:
+		while True:
+			#Wait for a connection
+			print("waiting")
+			conn,addr = s.accept()	#accept our connection and address of what we accepted, the program blocks on this line to a connection is received
+			print("accepting from ",addr)
+
+			#begin the threading to get it rolling		
+			t=threading.Thread(target=handle_client,args=(conn,addr))
+			t.start()
+			
+			#adding a thread
+			all_threads.append(t)
+			
+	except KeyboardInterrupt:
+		print("Stopped by Ctrl+C")
+	finally:
+		#do our cleanup
+		if s:
+			s.close()
+		for t in all_threads:
+			t.join()
 
 		
